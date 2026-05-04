4dc37ceabfd978581887dcfb14134940155948a7
Marina <marinacarracedo14@gmail.com>
2026-05-03 23:12:19 -0300

    flechas agregadas

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Nav } from './components/nav/nav'
 import { Comunidad } from './components/comunidad/comunidad';
 import { Hero } from './components/hero/hero';
-import { Noticias } from './components/noticias/noticias';
 import { Donaciones } from './components/donaciones/donaciones';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Nav, Comunidad, Hero, Noticias, Donaciones],
+  imports: [Nav, Comunidad, Hero, Donaciones],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
